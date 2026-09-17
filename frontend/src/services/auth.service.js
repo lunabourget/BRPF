6886f772fa8d518f0c4e66e46535f9d9997d9e05
lunabourget <lunabourget49@gmail.com>
2026-09-17 10:10:35 +0200

    change url backend access

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,7 +1,7 @@
 // Remplace par ton URL Render en production
 export const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:3000/api'
-  : process.env.BACKEND_URL;
+  : 'https://brpf.onrender.com/api';
 
 export const AuthService = {
   async login(email, password) {
